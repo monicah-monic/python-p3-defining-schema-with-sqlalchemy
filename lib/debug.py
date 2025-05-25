47c94b230a279203eeb3ee14468a3b030bcc7299
monicah-monic <monicahwanjiru205@gmail.com>
2025-05-25 15:25:00 +0300

    sqlalchemy schema

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import create_engine
+# from sqlalchemy import create_engine
 
-from sqlalchemy_sandbox import Student
+# from sqlalchemy_sandbox import Student
 
-engine = create_engine('sqlite:///students.db')
+# engine = create_engine('sqlite:///students.db')
 
-if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+# if __name__ == '__main__':
+#     import ipdb; ipdb.set_trace()
--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
+from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (create_engine,Column, Integer, String, desc,CheckConstraint,
+    PrimaryKeyConstraint, UniqueConstraint, Index, DateTime)
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 Base = declarative_base()
 
 class Student(Base):
-    pass
+    __tablename__ = 'students'
+
+    id = Column(Integer(), primary_key=True)
+    name= Column(String())
 
 if __name__ == '__main__':
-    pass
+    engine = create_engine('sqlite:///students.db')
+    Base.metadata.create_all(engine)
+
+      
+    Session = sessionmaker(bind=engine)  # use our engine to configure a 'Session' class
+    Session = Session  # use 'Session' class to create 'session' object
+ 
+ 
